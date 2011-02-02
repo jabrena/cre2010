6a4eb05bf5c52f200e36e3e35555206f83e0e97f
miferco97@gmail.com <miferco97@gmail.com@24dda4f5-2457-9293-027e-4e1bb11d5397>
2011-02-02 18:06:41 +0000

    Este es el primer programa que utiliza SVN

---
+++ trunk/miguel/Sesion2/src/Hola.java
@@ -0,0 +1,11 @@
+
+import lejos.nxt.*;
+
+public class Hola {
+  public static void main (String[] args) {
+    System.out.println("Hola Vida");
+    //muestra en la pantalla el texto selecionado
+    Button.waitForPress();
+    //espera hasta que se pulse un boton
+  }
+}
