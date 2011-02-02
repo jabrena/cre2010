38574950ee99bfc33f5c3cd012f6d58913e6d4a5
archivojab <archivojab@24dda4f5-2457-9293-027e-4e1bb11d5397>
2011-02-02 19:18:45 +0000

    La segunda sesión de la clase. Les ha costado pero progresan

---
+++ trunk/jab/robocupjr/sesion2/src/HelloWorld.java
@@ -0,0 +1,17 @@
+
+//Las librerias del robot
+import lejos.nxt.*;
+
+//Cada Robot tiene un programa
+//El robot se llama hello world
+public class HelloWorld {
+	
+	//El programa principal
+	public static void main (String[] args) {
+		
+		//AQUI SE PONEN LOS COMANDOS
+		System.out.println("Hello World");
+		Button.waitForPress();
+		
+	}
+}
