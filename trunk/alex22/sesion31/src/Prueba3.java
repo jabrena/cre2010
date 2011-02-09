71c2fbcb82d68f3df3d85f5c19d17fd3fae81d03
archivojab <archivojab@24dda4f5-2457-9293-027e-4e1bb11d5397>
2011-02-09 23:09:15 +0000

    Clase de TachoPilot

--- trunk/alex22/sesion31/src/Prueba3.java
+++ trunk/alex22/sesion31/src/Prueba3.java
@@ -2,28 +2,25 @@ import lejos.nxt.*;
 import lejos.robotics.navigation.*;
  
 /**
-* Clone Wars, Chapter 2
-* @author Alvaro & Irene
+* Clone Wars, Chapter 1
+* @author Alex1 & Alex2
 */
-public class Prueba3
-{
-public static void main( String[] args)
-{
- 
-//float cm = inches * 2.54f;
-Button.waitForPress();
- 
-float wheeldiameter = 5.5f;
-float wheelseparation = 10.5f;
- 
-Pilot pilot = new TachoPilot( wheeldiameter, wheelseparation, Motor.A, Motor.C, false);
-//pilot.forward();
-pilot.travel(120);
-pilot.rotate(90);
-pilot.travel(186);
-pilot.rotate(-108);
-pilot.travel(300);
-pilot.rotate(-108);
-pilot.travel(40);
-}
+public class Prueba3{
+	public static void main( String[] args){
+	 
+		Button.waitForPress();
+		 
+		float wheeldiameter = 5.5f;
+		float wheelseparation = 10.5f;
+		 
+		Pilot pilot = new TachoPilot( wheeldiameter, wheelseparation, Motor.A, Motor.C, false);
+		//pilot.forward();
+		pilot.travel(120);
+		pilot.rotate(90);
+		pilot.travel(186);
+		pilot.rotate(-108);
+		pilot.travel(300);
+		pilot.rotate(-108);
+		pilot.travel(40);
+	}
 }
