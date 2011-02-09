dad512f22a360a4f8f7a5c30fe5d85d49ca8f871
archivojab <archivojab@24dda4f5-2457-9293-027e-4e1bb11d5397>
2011-02-09 19:28:38 +0000

    Clase de TachoPilot

--- trunk/jab/robocupjr/sesion3/src/Prueba3.java
+++ trunk/jab/robocupjr/sesion3/src/Prueba3.java
@@ -7,29 +7,19 @@ import lejos.robotics.navigation.*;
 */
 public class Prueba3
 {
-public static void main( String[] args)
-{
- 
-//float cm = inches * 2.54f;
-Button.waitForPress();
- 
-float wheeldiameter = 5.5f;
-float wheelseparation = 8.5f;
- 
-Pilot pilot = new TachoPilot( wheeldiameter, wheelseparation, Motor.A, Motor.C, true);
-//pilot.forward();
-pilot.travel(-165f);
-pilot.rotate(-100);
-pilot.travel (-35f);
-pilot.rotate(100);
-pilot.travel (-170f);
-pilot.rotate (100);
-pilot.travel (-45f);
-pilot.rotate (-100);
-pilot.travel (-40);
-pilot.rotate (-100);
-pilot.travel (-40);
-//pilot.travel(-12,true);
- 
-}
+	public static void main( String[] args)
+	{
+	 
+	//float cm = inches * 2.54f;
+	Button.waitForPress();
+	 
+	float wheeldiameter = 5.5f;
+	float wheelseparation = 8.5f;
+	 
+	Pilot pilot = new TachoPilot( wheeldiameter, wheelseparation, Motor.A, Motor.C, false);
+	pilot.travel(165);
+	pilot.rotate(100);
+
+	 
+	}
 }
