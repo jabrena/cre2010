05641e32a02e9c9bd183aedd2c50290699d51860
alnicopri@hotmail.com <alnicopri@hotmail.com@24dda4f5-2457-9293-027e-4e1bb11d5397>
2011-03-18 18:13:26 +0000

    Mi tercer programa

--- trunk/miguel/RobocupJr/src/tiltsensor.java
+++ trunk/miguel/RobocupJr/src/tiltsensor.java
@@ -1,12 +1,21 @@
 import lejos.nxt.*;
 import lejos.nxt.addon.*;
 
-
 public class tiltsensor {
-       				
-		public void main(String[] args) throws Exception {
+     		
+static TiltSensor tilt = new TiltSensor(SensorPort.S3);
+public static int tiltvalue = 0;
+
+		public static void main(String[] args) {
+			
 			while (!Button.ESCAPE.isPressed()){
+				tiltvalue = 0;
+				LCD.clear();		
+				tiltvalue = tilt.getXTilt();
+				System.out.println(tiltvalue);					
+				Button.waitForPress();
+				try {Thread.sleep(1000);} catch (Exception e) {} 
 					
 			}
 		}	
-	}
+	} 
