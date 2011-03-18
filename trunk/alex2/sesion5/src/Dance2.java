7e1a50d1883d98710506b0cb809d4f1cb90779a7
alexelde5a@hotmail.com <alexelde5a@hotmail.com@24dda4f5-2457-9293-027e-4e1bb11d5397>
2011-03-18 19:08:31 +0000

    Programa terminado

--- trunk/alex2/sesion5/src/Dance2.java
+++ trunk/alex2/sesion5/src/Dance2.java
@@ -1,7 +1,14 @@
 		
-		import lejos.nxt.Motor;
+		import java.util.Random;
+
+import lejos.nxt.Button;
+import lejos.nxt.LCD;
+import lejos.nxt.LightSensor;
+import lejos.nxt.Motor;
+import lejos.nxt.SensorPort;
+import lejos.nxt.Sound;
 		import lejos.nxt.SoundSensor;
-		import lejos.robotics.navigation.TachoPilot;
+import lejos.robotics.navigation.TachoPilot;
 
 
 		public class Dance2 {
@@ -18,33 +25,19 @@
 			
 			//Interaccion
 			private static Motor armMotor;
-			
+			private static LightSensor light;
 			private static SoundSensor sound;
 			
-			/**
-			 * @param args
-			 */
-			public static void main(String[] args) {
-				// TODO Auto-generated method stub
-
-				//Instanciar
-				leftMotor = Motor.A;
-				rightMotor = Motor.C;
-				armMotor = Motor.B; 
-				leftMotor.setPower(100);
-				rightMotor.setPower(100);
-				pilot = new TachoPilot(wheeldiameter, wheelseparation, leftMotor, rightMotor, false);
-		        pilot.setSpeed(500);
+			private static void bloque1(){
 				
-				armMotor = Motor.B;
+				Sound.beep();
 				
 				//1.Espera de 3 segundos
-				int miliseconds = 3000;
-				try {Thread.sleep(miliseconds);} catch (Exception e) {}
+				int miliseconds = 0;
 				//2.Giro hacia el lado derecho mientras avanza
-				pilot.travelArc(25, 10);
+				pilot.travelArc(12, 10);
 				//3.Vuelve a la posicion inicial
-				pilot.travelArc(-25, -10);	
+				pilot.travelArc(-12, -10);	
 				//4.Espera de 1 segundo
 				miliseconds = 1000;
 				//5.Gira hacia el lado izquierdo mientras avanza
@@ -52,9 +45,9 @@
 				//6.Vuelve nuevamente a la posicion inicial
 				pilot.travelArc(12, 10);
 				//2.Giro hacia el lado derecho mientras avanza
-				pilot.travelArc(25, 10);
+				pilot.travelArc(12, 10);
 				//3.Vuelve a la posicion inicial
-				pilot.travelArc(-25, -10);	
+				pilot.travelArc(-12, -10);	
 				//4.Espera de 1 segundo
 				miliseconds = 1000;
 				//5.Gira hacia el lado izquierdo mientras avanza
@@ -62,15 +55,23 @@
 				//6.Vuelve nuevamente a la posicion inicial
 				pilot.travelArc(12, 10);
 				//2.Giro hacia el lado derecho mientras avanza
-				pilot.travelArc(25, 10);
+				pilot.travelArc(12, 10);
 				//3.Vuelve a la posicion inicial
-				pilot.travelArc(-25, -10);	
+				pilot.travelArc(-12, -10);	
 				//4.Espera de 1 segundo
 				miliseconds = 1000;
 				//5.Gira hacia el lado izquierdo mientras avanza
 				pilot.travelArc(-12, -10);
 				//6.Vuelve nuevamente a la posicion inicial
 				pilot.travelArc(12, 10);
+			}
+			
+			private static void bloque2(){
+				
+				Sound.beep();
+				
+				int miliseconds = 0;
+				
 				//7.Hace un giro de 270º
 				pilot.rotate(275);
 				try {Thread.sleep(miliseconds);} catch (Exception e) {}
@@ -91,6 +92,14 @@
 				armMotor.forward();
 				miliseconds = 100;
 				try {Thread.sleep(miliseconds);} catch (Exception e) {}
+			}
+			
+			private static void bloque3(){
+				
+				Sound.beep();
+				
+				int miliseconds = 0;
+				
 				//14.Hace un giro de 30º
 				pilot.rotate(30);
 				//15.Espera de 0'3 segundos
@@ -151,8 +160,17 @@
 				//37.Realiza una espera de 1 segundo
 				miliseconds = 1000;
 				try {Thread.sleep(miliseconds);} catch (Exception e) {}
+				
 				//38.El motor de los brazos para
 				armMotor.stop();
+			}
+			
+			private static void bloque4(){
+				
+				Sound.beep();
+				
+				int miliseconds = 0;
+				
 				//39.Realiza 4 vueltas
 				pilot.rotate(1455);
 				//40.Hace una espera de 0'5 segundos
@@ -172,6 +190,14 @@
 				try {Thread.sleep(miliseconds);} catch (Exception e) {}		
 				//46.Los motores A y B se paran
 				pilot.stop();
+			}
+			
+			private static void bloque5(){
+				
+				Sound.beep();
+				
+				int miliseconds = 0;
+				
 				//47.Gira los brazos hacia atras 5,5 segundos
 				armMotor.backward();
 				miliseconds = 5500;
@@ -183,11 +209,12 @@
 				//50.Gira 360º hacia el otro lado
 				pilot.rotate(-360);
 				//51.Giro hacia el lado derecho mientras avanza
-				pilot.travelArc(25, 10);
+				pilot.travelArc(12, 10);
 				//52.Vuelve a la posicion inicial
-				pilot.travelArc(-25, -10);	
+				pilot.travelArc(-12, -10);	
 				//53.Espera de 1 segundo
 				miliseconds = 1000;
+				try {Thread.sleep(miliseconds);} catch (Exception e) {}
 				//54.Gira hacia el lado izquierdo mientras avanza
 				pilot.travelArc(-12, -10);
 				//55.Vuelve nuevamente a la posicion inicial
@@ -196,69 +223,121 @@
 				armMotor.forward();
 				miliseconds = 1000;
 				try {Thread.sleep(miliseconds);} catch (Exception e) {}
-				
-				
-				
-				
-				
-				
-				
-				
-				
-				
-			
-			
-			
-			
-			
-			
-			
-			
-			
-			
-			
-			
-			
-			
-			
-			
-			
-			
-			
-			
-			
-			
-			
-			
-			
-			
-			
-			
-			
-			
-			
-			
-			
-			
-			
-			
-			
-			
-			
-			
-			
-			
-			
-			
-			
-			
-			
-			
-			
-			
+				//57.Los brazos paran
+				armMotor.stop();
 			}
 			
+			private static void bloque6(){
+				
+				Sound.beep();
+				
+				int miliseconds = 0;
+
+				//58.Giro hacia el lado derecho mientras avanza
+				pilot.travelArc(20, 30);
+				//59.Giro hacia el lado derecho mientras avanza
+				pilot.travelArc(20, 30);
+				//60.Giro hacia el lado derecho mientras avanza
+				pilot.travelArc(20, 30);
+				//61.Giro hacia el lado derecho mientras avanza
+				pilot.travelArc(20, 30);
+				//62.Giro hacia el lado derecho mientras avanza
+				pilot.travelArc(20, 30);
+				//63.Giro hacia el lado derecho mientras avanza
+				pilot.travelArc(20, 30);
+				//64.Giro hacia el lado derecho mientras avanza
+				pilot.travelArc(20, 30);
+				//65.Giro hacia el lado derecho mientras avanza
+				pilot.travelArc(20, 30);
+				//66.Giro hacia el lado derecho mientras avanza
+				pilot.travelArc(20, 30);
+				//67.Giro hacia el lado derecho mientras avanza
+				pilot.travelArc(20, 30);
+				//68.Giro hacia el lado derecho mientras avanza
+				pilot.travelArc(20, 30);
+				//69.Giro hacia el lado derecho mientras avanza
+				pilot.travelArc(20, 30);
+				//70.Giro hacia el lado derecho mientras avanza
+				pilot.travelArc(20, 30);
+				//71.Giro hacia el lado derecho mientras avanza
+				pilot.travelArc(20, 30);
+				//72.Giro hacia el lado derecho mientras avanza
+				pilot.travelArc(20, 30);
+				//73.Giro hacia el lado derecho mientras avanza
+				pilot.travelArc(20, 30);
+				//74.Giro hacia el lado derecho mientras avanza
+				pilot.travelArc(20, 30);
+				//75.Giro hacia el lado derecho mientras avanza
+				pilot.travelArc(20, 30);
+				//76.Giro hacia el lado derecho mientras avanza
+				pilot.travelArc(20, 30);
+
+			}
 			
+			public static void main(String[] args) {
+
+				//Instanciar
+				leftMotor = Motor.A;
+				rightMotor = Motor.C;
+				armMotor = Motor.B; 
+				leftMotor.setPower(100);
+				rightMotor.setPower(100);
+				pilot = new TachoPilot(wheeldiameter, wheelseparation, leftMotor, rightMotor, false);
+		        pilot.setSpeed(500);
+				
+				armMotor = Motor.B;
+				
+				sound = new SoundSensor(SensorPort.S2);
+				light = new LightSensor(SensorPort.S3);
+				int soundValue = 0;
+				
+				/*
+				while(!Button.ENTER.isPressed()){
+					
+					soundValue = sound.readValue();
+					LCD.drawString("V:"+ soundValue, 0,3);
+					
+					int miliseconds = 100;
+					
+					if(soundValue > 50){
+						armMotor.forward();
+						try {Thread.sleep(miliseconds);} catch (Exception e) {}
+						armMotor.stop();
+						armMotor.backward();
+						try {Thread.sleep(miliseconds);} catch (Exception e) {}
+						armMotor.stop();
+						luz();
+					}
+				}
+				*/
+
+				bloque1();
+				bloque2();
+				bloque3();
+				bloque4();
+				luz();
+				bloque5();
+				luz();
+				bloque6();
+				
+			}
+			
+			private static void While(boolean b) {
+				// TODO Auto-generated method stub
+				
+			}
+
+			private static void luz(){
+				Random random = new Random();
+				int randomValue = 0;
+				randomValue = random.nextInt(10);
+				
+				if(randomValue > 5){
+					light.setFloodlight(true);
+				}else{
+					light.setFloodlight(false);
+				}
+			}
 			
 
 	}
