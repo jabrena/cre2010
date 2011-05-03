b69829d31d578733a11f48b07bc9645df9082b21
mrnogues89@gmail.com <mrnogues89@gmail.com@24dda4f5-2457-9293-027e-4e1bb11d5397>
2011-05-03 19:59:27 +0000

    Robot - Mejora del uso de las clases.

--- mruiz/sources/src/Navegar.java
+++ mruiz/sources/src/Navegar.java
@@ -1,3 +1,6 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
 import lejos.nxt.LightSensor;
 
 /*
@@ -10,7 +13,9 @@ public class Navegar {
 	//Atributos:
 	LightSensor light;
 	Producto productos [];
-	
+    //Lectura por Teclado:
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in)); 
+    String linea;   
 	//Constructores:
 	
 	public Navegar (LightSensor light, Producto productos []){
@@ -20,12 +25,17 @@ public class Navegar {
 		
 	}
 	//Métodos:
-	
+	/*
+	 * Identificat Posición.
+	 * 
+	 * Devuelve la posición del vector de productos donde se encuentra el robot, o -1 si no la conoce.
+	 */
 	public int identificar_posicion (){
+		
 		int posicion=-1;
 		   	for (int i=0;i<4;i++)
         	{
-        		if(productos[i].comprobar_producto(light.readValue())==true)
+        		if(productos[i].get_Color()==light.readValue())
             	{
             		
             		posicion=i;
@@ -38,4 +48,41 @@ public class Navegar {
 	public void generar_trayectoria (){
 		
 	}
+	/*
+	 * DESTINO
+	 * 
+	 * Devuelve el elemento al que se desea ir.
+	 */
+	
+	public int destino (){
+
+
+        	int opc=0;
+        	do{
+        		System.out.println("Escoga el producto.");
+        		System.out.println("1.Pan");
+            	System.out.println("2.Leche");
+            	System.out.println("3.Cereales");
+            	System.out.println("4.Mermelada");
+            	System.out.println("           Opción:");
+            	
+            	try{
+                 	
+                	opc = Integer.parseInt(br.readLine())-1;
+                }
+                catch(Exception e){ 
+                	
+                	e.printStackTrace();
+                }  
+                
+                if(opc<0 || opc>3)
+                {
+                	System.out.println("Introduzca el resultado de nuevo.");
+                }
+                
+        	}while(opc<0 || opc>3);
+        	
+        	return opc;
+     }
+	
 }
